ad3ed484d61559d4cfdcef48fc30c292a4e3494d
Antonio Carlos Fernandes Gomes Filho <antoniocarlosfilho2003@gmail.com>
2024-02-09 16:57:05 -0300

    Delete pyRegexGroup.py

--- pyRegexGroup.py
+++
@@ -1,6 +0,0 @@
-import re
-phone_num = re.compile('(\(\d{2}\)) (\d{5}-\d{4})')
-Result = phone_num.search("Numero telefonico é (87) 99756-1234. Ligue as 19hrs.")
-print('DDD: ' + Result.group(1))
-print('Telefone: '+ Result.group(2))
-print('DDD e Telefone: ' + Result.group(0))
