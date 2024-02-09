47dd53808b57ab096f69f0e4618285ee2a07a383
Antonio Carlos Fernandes Gomes Filho <antoniocarlosfilho2003@gmail.com>
2024-02-09 16:57:21 -0300

    Delete pyRegexOcorrencia.py

--- pyRegexOcorrencia.py
+++
@@ -1,6 +0,0 @@
-import re
-phone_num = re.compile('(\(\d{2}\) )?(\d{5}-\d{4})')
-mol = phone_num.search("Numero telefonico é (87) 99756-1234. Ligue as 19hrs.")
-print(mol.group())
-mol = phone_num.search("Numero telefonico é 99756-1234. Ligue as 19hrs.")
-print(mol.group())
