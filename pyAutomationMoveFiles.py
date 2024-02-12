211412948358545befd30951dec4bdb9277da678
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2024-02-12 20:04:33 -0300

    Update: Uso do caminho relativo no metodo Path() e refatorando variaveis

--- pyAutomationMoveFiles.py
+++ pyAutomationMoveFiles.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 import shutil
 
+
 def get_file_count(directory, prefix):
     count = 0
     for filename in os.listdir(directory):
@@ -9,27 +10,30 @@ def get_file_count(directory, prefix):
             count += 1
     return count
 
-def move_files(sourceDirectory, destinationDirectory, prefix):
-    if not os.path.exists(destinationDirectory):
-        os.makedirs(destinationDirectory)
-    for filename in os.listdir(sourceDirectory):
+
+def move_files(source_directory, destination_directory, prefix):
+    if not os.path.exists(destination_directory):
+        os.makedirs(destination_directory)
+    for filename in os.listdir(source_directory):
         if filename.startswith(prefix) and filename.endswith(".py"):
-            source_file = os.path.join(sourceDirectory, filename)
-            dest_file = os.path.join(destinationDirectory, filename)
+            source_file = os.path.join(source_directory, filename)
+            dest_file = os.path.join(destination_directory, filename)
             shutil.move(source_file, dest_file)
-            print(f"Moved {filename} to {destinationDirectory}")
+            print(f"Moved {filename} to {destination_directory}")
+
 
 def main():
-    sourceDirectory = Path("c:/Users/admin/Desktop/python_Antonio")
+    source_directory = Path("../pyUdemy")
 
-    pyRegex_count = get_file_count(sourceDirectory, "pyRegex")
-    pyFile_count = get_file_count(sourceDirectory, "pyFile")
+    py_regex_count = get_file_count(source_directory, "pyRegex")
+    py_file_count = get_file_count(source_directory, "pyFile")
 
-    print(f"Found {pyRegex_count} pyRegex files\n")
-    print(f"Found {pyFile_count} pyFile files\n")
+    print(f"Found {py_regex_count} pyRegex files\n")
+    print(f"Found {py_file_count} pyFile files\n")
 
-    move_files(sourceDirectory, sourceDirectory / "pyRegex", "pyRegex")
-    move_files(sourceDirectory, sourceDirectory / "pyFile", "pyFile")
+    move_files(source_directory, source_directory / "pyRegex", "pyRegex")
+    move_files(source_directory, source_directory / "pyFile", "pyFile")
+
 
 if __name__ == "__main__":
     main()
