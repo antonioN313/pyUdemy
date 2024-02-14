179dfbd332c1f2e98b64b2b02261511b5850ff76
antonioN313 <47403160+antonioN313@users.noreply.github.com>
2024-02-13 23:57:27 -0300

    update: pyPDF directory added

--- pyAutomationMoveFiles.py
+++ pyAutomationMoveFiles.py
@@ -28,15 +28,17 @@ def main():
     py_regex_count = get_file_count(source_directory, "pyRegex")
     py_file_count = get_file_count(source_directory, "pyFile")
     py_excel_count = get_file_count(source_directory, "pyExcel")
+    py_pdf_count = get_file_count(source_directory, "pyPDF")
 
     print(f"Found {py_regex_count} pyRegex files\n")
     print(f"Found {py_file_count} pyFile files\n")
     print(f"Found {py_excel_count} pyFile files\n")
+    print(f"Found {py_pdf_count} pyFile files\n")
 
     move_files(source_directory, source_directory / "pyRegex", "pyRegex")
     move_files(source_directory, source_directory / "pyFile", "pyFile")
     move_files(source_directory, source_directory / "pyExcel", "pyExcel")
-
+    move_files(source_directory, source_directory / "pyPDF", "pyPDF")
 
 
 if __name__ == "__main__":
